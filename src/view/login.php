19a811cc19b39d2d38eeab633091a11f5a142a6d
Bianca Jussara Wolff <biancajwff@gmail.com>
2025-03-21 16:01:54 -0300

    Testando as branchs

--- src/view/login.php
+++ src/view/login.php
@@ -1,22 +1,3 @@
-<?php
-require '../controller/config.php';
-
-if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-    $email = $_POST['email'];
-    $senha = $_POST['senha'];
-
-    $sql = "SELECT * FROM usuarios WHERE email = ?";
-    $stmt = $pdo->prepare($sql);
-    $stmt->execute([$email]);
-    $user = $stmt->fetch(PDO::FETCH_ASSOC);
-
-    if ($user && password_verify($senha, $user['senha'])) {
-        echo "Login bem-sucedido!";
-    } else {
-        echo "Email ou senha incorretos.";
-    }
-}
-?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -26,7 +7,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 </head>
 <body>
 
-<form method="POST">
+<form method="POST" action="../controller/controller_login.php">
     Email: <input type="email" name="email" required><br>
     Senha: <input type="password" name="senha" required><br>
     <button type="submit">Entrar</button>
