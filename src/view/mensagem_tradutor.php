66fe499c8369b3745c1a7d3ad5306825daaf82ae
Ana Beatriz <anabeatrizkraener@gmail.com>
2025-06-20 00:27:49 -0300

    Arrumando os pop-ups de mensagem do tradutor

--- src/view/mensagem_tradutor.php
+++ src/view/mensagem_tradutor.php
@@ -11,15 +11,17 @@
 
 <body class="flex flex-col gap-20 justify-center items-center h-screen">
 
-<!--Design do pop-up de mensagens-->
+<div class="relative">
+    <!--Design do pop-up de mensagens-->
 <div>
     <span class="py-2 px-7 rounded-2xl rounded-br-none bg-[#F8FBA6] text-xl text-[#746587] font-bold" style="box-shadow: -5px 5px 0px #746587"><!--Frase traduzida vêm aqui--></span>
 </div>
 
 <!--Design do perfil do usuario-->
-<div class="text-center">
+<div class="text-center absolute -right-10 top-12">
     <img src="../imgs/icones/perfilMensagem.png" class="bg-white p-2 rounded-2xl" alt="Perfil do usuário">
     <span class="text-sm text-[#746587] font-bold"><!--Nome do usuário que está mandando a mensagem fica aqui--></span>
+</div>
 </div>
 
 </body>
